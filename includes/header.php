5323128f8532a7a8624a65b9585299aae4cff1f8
pistell <jamespistell@gmail.com>
2016-05-10 16:51:13 -0400

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -10,9 +10,9 @@ require_once('functions.php');
 <head>
     <meta charset="utf-8">
     <title>Home</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" media="screen" type="text/css" href="css/datepicker.css">
     <link rel="stylesheet" media="screen" type="text/css" href="css/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 </head>
-<body class="<?php echo get_body_class(); ?>">
+<body class="<?php echo get_active_page(); ?>">
