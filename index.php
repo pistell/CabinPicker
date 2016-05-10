7af3c49211bf55ab3da8db113bbf5357dfc77c39
pistell <jamespistell@gmail.com>
2016-05-10 05:03:10 -0400

    vacation packages added

--- index.php
+++ index.php
@@ -51,57 +51,66 @@ require_once( 'includes/nav.php');
           <div class="showImage">
             <img class="img-fluid event-img" alt="The Nashua Chorus of New England" src="img/orchestra.jpg" />
             <!-- Opens a new tab, not needed in this assignment -->
-            <input type="button" class="hoverButton btn btn-primary" onclick="window.open('http://google.com','_blank')" value="Discover">
+            <input type="button" class="hoverButton btn btn-primary" value="Discover">
           </div>
 
 
          </div>
          <h4 class="eventTitle text-justify">63rd Annual Show</h4>
-         <p class="lead"> Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small> </p>
+         <p class="lead"> The New England Chorus of Nashua, <strong>New Hampshire</strong>. Proudly Invites you to the 63rd Annual Concert <em>"That's Life</em>," featuring special guest quartet Throwback. Conductor Ernst Junger will be Celebrating the 20th anniversary of winning the <em>Van Cliburn Competition</em>, he returns for Rachmaninoff’s popular and passionate concerto. <small>The festivities continue with Beethoven’s spirited Symphony No. 7, featuring the haunting Allegretto that’s captivated listeners from concert hall to movie screen.</small> </p>
+
+         <!-- Event 2-->
+         <div class="events">
+           <!-- Get the current date and add 3 days -->
+          <h4 class="eventDate text-center"><span><?php echo date("F j, Y", strtotime("+10 days"));?></span></h4>
+
+          <div class="showImage">
+            <img class="img-fluid event-img" alt="Louis Barthas International Photography Exhibit" src="img/photography.jpg" />
+            <!-- Opens a new tab, not needed in this assignment -->
+            <input type="button" class="hoverButton btn btn-primary" value="Discover">
+          </div>
+
+
+         </div>
+         <h4 class="eventTitle text-justify">Louis Barthas Photography Exhibit</h4>
+         <p class="lead"> A selection of Louis Barthas' large format Silver Prints will be on display in <strong>ARTISAN CO-OP</strong> in Bethel, Maine. Barhas is a Fanny Knapp Allen Professor Emeritus of Art History, and Artist-in-Residence, at the University of Rochester. He is the author of numerous essays and of the critical biography, <em>Aaron Siskind: Pleasures and Terrors (Little, Brown, Boston, 1982)</em>. His photographs have been seen in over <em>80 one-person, and in over 260 group exhibitions</em> since 1957!<small> OPENING RECEPTION <?php echo date("F j, Y", strtotime("+10 days"));?> from 6-9p.m. $12 Fee to general public and as always ARTISAN CO-OP members gain free admission.</small></p>
+
          <!--END LOCAL EVENTS -->
-
-
-
      </div>
 		<div class="col-md-4">
-			<h3 class="bottomHeadlines text-center">
-            Vacation Packages
-         </h3>
-			<div class="row">
-				<div class="col-md-4">
-					<div class="thumbnail"> <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/g/200/200/" />
-						<div class="caption">
-							<h3>
-                        Thumbnail label
-                     </h3>
-							<p> Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. </p>
-							<p> <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a> </p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="thumbnail"> <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/g/200/200/" />
-						<div class="caption">
-							<h3>
-                        Thumbnail label
-                     </h3>
-							<p> Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. </p>
-							<p> <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a> </p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="thumbnail"> <img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/g/200/200/" />
-						<div class="caption">
-							<h3>
-                        Thumbnail label
-                     </h3>
-							<p> Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. </p>
-							<p> <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a> </p>
-						</div>
-					</div>
-				</div>
-			</div>
+      <!-- Vacation Packages -->
+      <h3 class="bottomHeadlines text-center">Vacation Packages</h3>
+      <div class="events">
+        <!-- Get the current date and add 3 days -->
+       <h4 class="eventDate text-center"><span>Open April through September</span></h4>
+
+       <div class="showImage">
+         <img class="img-fluid event-img" alt="Hearth and Timber Wine Tour Package" src="img/wine.jpg" />
+         <!-- Opens a new tab, not needed in this assignment -->
+         <input type="button" class="hoverButton btn btn-primary" value="Discover">
+       </div>
+
+
+      </div>
+      <h4 class="eventTitle text-justify">New England Wine Tours</h4>
+      <p class="lead">The Hearth and Timber Co. Wine Tour winds through the heart of the <strong>Southeastern New England</strong>. This particular tour is paired with our <a href="reservations.php?cabin=3"><em>Little Pond cabin in Nashua, NH</em></a>. Spend two to three days winding through historic cities and charming villages to visit the fourteen locally owned wineries that call this region home. <small>Call for group pricing when you reserve your cabin</small>.</p>
+
+      <!--Vacation Package 2-->
+      <div class="events">
+        <!-- Get the current date and add 3 days -->
+       <h4 class="eventDate text-center"><span>Available Year Round</span></h4>
+
+       <div class="showImage">
+         <img class="img-fluid event-img" alt="Hearth and Timber Executive Retreats" src="img/corporate.jpg" />
+         <!-- Opens a new tab, not needed in this assignment -->
+         <input type="button" class="hoverButton btn btn-primary" value="Discover">
+       </div>
+
+
+      </div>
+      <h4 class="eventTitle text-justify">Rustic Executive Retreats</h4>
+      <p class="lead">Rustic Executive Retreats by The Hearth and Timber Co. <strong>will rejuvenize your company leadership</strong>. Whether your retreat is a <em>half-day, full-day or a multi-day event</em>, Hearth and Timber Executive Retreats can help you make the most of your stay. Hosting a large group? Looking for exclusive use of any of our <a href="/cabins.php">cabins</a>? Let us help plan your <em>executive retreat to maximize productivity and fun</em> in this unique setting straddling the Colorado-Wyoming border. <small>Making good use of the panoramic mountain scenery, Three Forks Ranch offers flexible, functional indoor and outdoor spaces — providing breathtaking venues for corporate gatherings, business events and executive retreats.</small></p>
+
 		</div>
 		<div class="col-md-4">
 			<h3 class="bottomHeadlines text-center">
