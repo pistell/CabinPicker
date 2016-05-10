62934dd5b5523c83a839134046bacd5aa1e6ee77
pistell <jamespistell@gmail.com>
2016-05-10 10:21:01 -0400

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,10 +3,18 @@
  * Our Footer
  */
 ?>
-<hr>
-<p class="text-center">
-  Site by James Pistell
-</p>
+
+<footer id="footer">
+    <div class="container-fluid text-center">
+
+        <div class="social__item">
+          <a target="_blank" href="http://instagram.com/hearthandtimber" class="social__icon--instagram"><i class="icon--instagram"></i></a>
+        </div>
+
+        <p class="text-center">Site by James Pistell</p>
+
+    </div>
+</footer>
 
 <!-- Moved jQuery to header.php-->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
