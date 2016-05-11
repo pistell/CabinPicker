3b3eb94a0b7ca92ef9cba70392a2bd8a96845150
pistell <jamespistell@gmail.com>
2016-05-11 16:14:52 -0400

    programmatically added meta tags in PHP

--- includes/functions.php
+++ includes/functions.php
@@ -25,3 +25,42 @@ function get_active_page() {
 
     return $basename;
 }
+//Meta keywords
+function get_meta_keywords(){
+
+  $basename = basename( $_SERVER['PHP_SELF'], ".php" );
+  if ($basename == 'cabins') {
+      //Add keywords
+      $keywords = array('cabin rentals', 'new england cabins', 'vacation', 'log cabins', 'cottages');
+      //Separate array by commas and remove brackets
+      $clean_keywords = implode(", ", $keywords);
+  } elseif ($basename == 'reservations') {
+    $keywords = array('reserve cabins', 'log cabin booking', 'book vacation', 'booking', 'calendar');
+    $clean_keywords = implode(", ", $keywords);
+  } else {
+    $keywords = array('home', 'index', 'Hearth and Timber', 'log cabin rentals', 'log cabin company', 'log cabin vacations');
+    $clean_keywords = implode(", ", $keywords);
+  }
+
+  return $clean_keywords;
+}
+//Vars inside function cannot be accessed from other files
+$keywordsToEcho = get_meta_keywords();
+
+// Meta description
+function get_meta_desc(){
+  $basename = basename( $_SERVER['PHP_SELF'], ".php" );
+  if ($basename == 'cabins') {
+      //Add description
+      $desc = "Hearth and Timber log cabin rental company offers several log cabins to choose from throughout the New England region";
+  } elseif ($basename == 'reservations') {
+      $desc = "Reserve your cabin today through the Hearth and Timber Cabin Company's website. Prices vary on season";
+  } else {
+      $desc = "Welcome to the Hearth and Timber Cabin Company website. Browse through our cabins and vacation packages. Proudly serving the New England region. Family owned and operated since 1985";
+  }
+
+  return $desc;
+}
+//Vars inside function cannot be accessed from other files
+$descToEcho = get_meta_desc();
+?>
--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,10 @@ require_once('functions.php');
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="description" content="<?php print_r($descToEcho); ?>">
+    <!-- Quotation marks removed due to json_encode printing them -->
+    <meta name="keywords" content=<?php echo json_encode($keywordsToEcho); ?>>
+    <meta name="author" content="James Pistell">
     <!-- Capitalize the first letter of the $basename var from get_active_page() -->
     <title><?php echo ucfirst(get_active_page($basename)); ?> | Hearth and Timber Co.</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
